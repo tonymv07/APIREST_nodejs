2994ec6ffb81e1d357bafc4d4dc81ced794b1023
Anthony Muñoz M <anthony-munozm@gmail.com>
2022-05-10 11:36:17 -0600

    edit

--- src/routes/students.js
+++ src/routes/students.js
@@ -4,67 +4,85 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 
 router.get('/', (req, res)=>{
-    mysqlConnection.query('SELECT * FROM students', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM posts', (err, rows, fields) => {
         if(!err){
             res.json(rows);
         
         }else{
-            console.log(err);
+            res.json(err);
         }
     });
 });
 
 router.get('/id=:id', (req, res)=>{
     const {id} = req.params;
-    mysqlConnection.query('SELECT * FROM students WHERE id = ?', [id], (err, 
+    mysqlConnection.query('SELECT * FROM posts WHERE id = ?', [id], (err, 
     rows, fields) => {
         if(!err){
             res.json(rows[0]);
         
         }else{
-            console.log(err);
+            res.json(err);
         }
     });
 });
 
 router.post('/', (req, res) => {
-    const { id, name, age } = req.body;
-    const query = `
-        CALL studentAddOrEdit(?, ?, ?);
-    `;
-    mysqlConnection.query(query, [id, name, age], (err, rows, fields) => {
+    const { id, title, status, content, user_id } = req.body;
+    
+    mysqlConnection.query('INSERT INTO posts (id,title,status,content,user_id) VALUES (?, ?, ?, ?, ?)', [id, title, status, content, user_id], (err, rows, fields) => {
         if(!err){
             res.json({Status: 'Student Saved'});
         
         }else{
-            console.log(err);
+            res.json(err);
         }
     });
 });
 
 router.put('/id=:id', (req, res) => {
-    const { name, age } = req.body;
     const { id } = req.params;
-    const query = 'CALL studentAddOrEdit(?, ?, ?)';
-    mysqlConnection.query(query, [id, name, age], (err, rows, fields) => {
-        if(!err){
-            res.json({status: 'Student Updated'});
+    mysqlConnection.query('SELECT * FROM posts WHERE id = ?', [id], (err, 
+        rows, fields) => {
+            if(!err){
 
-        }else{
+                if ("title" in req.body){
+                    rows[0]["title"] = req.body["title"]
+                }
+                if ("status" in req.body){
+                    rows[0]["status"] = req.body["status"]
+                }
+                if ("content" in req.body){
+                    rows[0]["content"] = req.body["content"]
+                }
+                if ("user_id" in req.body){
+                    rows[0]["user_id"] = req.body["user_id"]
+                }
+                const data = [rows[0]["title"], rows[0]["status"], rows[0]["content"], rows[0]["user_id"], rows[0]["id"]]
+                const { name } = req.body;
+                mysqlConnection.query('UPDATE posts SET title=?, status=?, content=?, user_id=? where id = ?', data, (err, rows, fields) => {
+                    if(!err){
+                        res.json({status: 'Student Updated'});
 
-            console.log(err);
-        }
-    });
+                    }else{
+
+                        res.json(err);
+                    }
+                });
+            }else{
+                res.json(err);
+            }
+        });
 });
 
 router.delete('/id=:id', (req, res) => {
     const { id } = req.params;
-    mysqlConnection.query('DELETE FROM students WHERE id = ?', [id], (err, rows, fiels) => {
+    mysqlConnection.query('DELETE FROM posts WHERE id = ?', [id], (err, rows, fiels) => {
         if(!err){
             res.json({status: 'Student Deleted'});
         
         }else{
-            console.log(err);
+            res.json(err);
         }
     });
 });
